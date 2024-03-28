035cd43c4fb29eb5e428e6adff891dccae21c980
henriqerocha <henriiqesilva@gmail.com>
2024-03-28 20:53:06 -0300

    adicionado botão para mudar tema escuro ou claro

--- js/script.js
+++ js/script.js
@@ -12,3 +12,31 @@ cards.forEach(card => {
   });
 });
 
+
+//mudar o tema para escuro ou claro
+let temaAtual = false;
+
+function mudarTema(){
+
+  const bgHeader = document.querySelector('.topo');
+  const link = document.querySelectorAll('.nav-link');
+
+  if(!temaAtual){
+    bgHeader.style.backgroundColor = '#191919';
+
+    link.forEach(() =>{
+      
+    });
+
+
+    console.log('escuro');
+  }else{
+    bgHeader.style.backgroundColor = '#fff';
+    console.log('claro');
+  }
+
+  temaAtual = !temaAtual;
+}
+
+let buttonTema = document.querySelector('.theme-switch__container');
+buttonTema.addEventListener("click", mudarTema);
